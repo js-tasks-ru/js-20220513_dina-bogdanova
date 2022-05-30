328ca5c35003e9436fdc6de7b2a315aa52f98a50
heyhurricane <dinohka@mail.ru>
2022-05-31 01:07:09 +0400

    add functions createGetter, trimSymbols, invertObj, uniq

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,15 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
-
+  if (obj) {
+    let newObj = Object.entries(obj);
+      for (let i = 0; i < newObj.length; i++) {
+        let newValue =  newObj[i][0];
+        newObj[i][0] = newObj[i][1];
+        newObj[i][1] = newValue;
+      }
+    newObj = Object.fromEntries(newObj);
+    return newObj;
+  }
+  else return;
 }
